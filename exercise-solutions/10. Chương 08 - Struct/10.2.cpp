daa8066bafe161815b4aebcfbaefff25e2db62a5
longpt271 <longpt271@gmail.com>
2023-11-18 01:53:16 +0700

    10. Chương 08 - Struct - Bai 2.2

--- exercise-solutions/10. Chương 08 - Struct/10.2.cpp
+++ exercise-solutions/10. Chương 08 - Struct/10.2.cpp
@@ -1,5 +1,6 @@
 // Bài 2: Viết chương trình quản lý sản phẩm (tên, đơn giá, số lượng) gồm các chức năng sau:
 // - Nhập xuất dữ liệu struct
+// - show_menu()
 
 #include <iostream>
 #include <string>
@@ -34,13 +35,59 @@ struct Product
 	}
 };
 
+int show_menu();
+
 
 int main()
 {
-	Product product;
-	
-	cin >> product;
-	cout << product;
-	
+	while(true)
+	{
+		int chose = show_menu();
+		
+		switch(chose)
+		{
+			case 1:
+				// Xem danh sach
+				break;
+			case 2:
+				// Them san pham
+				break;
+			case 3:
+				// Xoa san pham
+				break;
+			case 0:
+				cout << "Bye bye!";
+				
+				return 0;
+			default:
+				cout << "Lua chon khong hop le." << endl;	
+		}
+		
+		cout << "Ban co muon tiep tuc khong? (1/0) ";
+		bool is_continue;
+		cin >> is_continue;
+		
+		if(!is_continue)
+		{
+			cout << "Bye bye!";
+			break;
+		}
+	}
 	return 0;
 }
+
+int show_menu()
+{
+	system("cls");	// xoa toan bo du lieu tren man hinh console
+	int chose;
+	cout << "===== MENU =====" << endl;
+	cout << "1. Xem danh sach san pham" << endl;
+	cout << "2. Them san pham moi" << endl;
+	cout << "3. Xoa san pham" << endl;
+	cout << "0. Thoat chuong trinh" << endl;
+	
+	cout << "Lua chon cua ban la: ";
+	cin >> chose;
+	
+	return chose;
+ } 
